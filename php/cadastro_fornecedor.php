1495628590fe45ce37bc1a50a2f37bece4c432ea
Gazotti <vtr.gomes@hotmail.com>
2015-07-02 00:26:23 -0300

    php para sobrinhos

--- php/cadastro_fornecedor.php
+++ php/cadastro_fornecedor.php
@@ -15,3 +15,17 @@ $uf = $_POST['fornecedor_uf'];
 $pais = $_POST['fornecedor_pais'];
 $fone = $_POST['fornecedor_fone'];
 $email = $_POST['fornecedor_email'];
+
+$cnpj = limpar_caracteres($cnpj);
+
+print_r($cnpj);
+
+
+
+
+
+
+
+function limpar_caracteres($param){
+    return preg_replace('/\D/', '', $param);
+}
