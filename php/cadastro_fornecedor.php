3ecd02a4dc2bfe7f85dbc136bb1660b6bb620543
Gazotti <vtr.gomes@hotmail.com>
2015-07-03 00:19:42 -0300

    insert ok

--- php/cadastro_fornecedor.php
+++ php/cadastro_fornecedor.php
@@ -16,16 +16,41 @@ $pais = $_POST['fornecedor_pais'];
 $fone = $_POST['fornecedor_fone'];
 $email = $_POST['fornecedor_email'];
 
-$cnpj = limpar_caracteres($cnpj);
-
-print_r($cnpj);
-
-
-
-
+$cnpj = limparCaracteres($cnpj);
+if(!validarEmail($email)){
+    exit('Email digitado inválido');
+}
+$id = 3;
 
 
+    if($stmt = $db->prepare("INSERT INTO fornecedores(forn_id, forn_cnpj, forn_razaosoc, forn_rua, forn_numero, forn_complemento, forn_cep, forn_bairro,
+                                        forn_cidade, forn_uf, forn_pais, forn_fone, forn_email) VALUES (?, ?, ?, ?, ?, ? ,?, ? ,? ,? ,? ,? , ?)")) {
+        $stmt->bind_param('isssissssssss', $id, $cnpj, $razao, $rua, $numero, $complemento, $cep, $bairro, $cidade, $uf, $pais, $fone, $email);
 
-function limpar_caracteres($param){
+        if($stmt->execute()){
+            echo 'Inseriu OK';
+        }else{
+            die('Erro: ( '.$db->errno.' ) '. $db->error);
+        }
+
+    }else{
+        printf("Erro ao preparar statement: %s\n", $db->error);
+    }
+
+$stmt->close();
+
+
+
+
+function limparCaracteres($param){
     return preg_replace('/\D/', '', $param);
+}
+
+function validarEmail($email){
+    return  (preg_match("/^(([0-9a-zA-Z]+[-._+&])*[0-9a-zA-Z]+@([-0-9a-zA-Z]+[.])+[a-zA-Z]{2,6}){0,1}$/", $email) ? true : false);
+}
+
+function getLastId($nomeTabela){
+    global $db;
+    $db->query("SELECT forn_id FROM fornecedores WHERE id = 1")->fetch_object()->product_name;
 }
