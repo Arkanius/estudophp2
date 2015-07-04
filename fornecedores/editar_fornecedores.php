495b291aac41a8e38665727780eeb2a42969bfc2
Gazotti <vtr.gomes@hotmail.com>
2015-07-04 18:59:37 -0300

    php for dummies

--- fornecedores/editar_fornecedores.php
+++ fornecedores/editar_fornecedores.php
@@ -7,19 +7,8 @@
  */
 
 require('../config.php');
-
-$url = $_SERVER['REQUEST_URI'];
-$url = explode('/', $url);
-if(!empty($url[1])){
-
-}
-
+ 
 $result = $db->query('SELECT * FROM fornecedores WHERE forn_id = "'.$_GET["id"].'"')->fetch_array();
-
-function editarFornecedor($id){
-    global $db;
-
-}
 ?>
 
 
@@ -37,7 +26,7 @@ function editarFornecedor($id){
 
 <fieldset>
     <legend>Cadastro de fornecedores</legend>
-    <form id="form_fornecedor" name="form_fornecedor" method="POST" action="/edit">
+    <form id="form_fornecedor" name="form_fornecedor" method="POST" action="fornecedores/editar_fornecedores">
         <label>Cnpj: </label><input type="text" name="fornecedor_cnpj" id="fornecedor_cnpj" placeholder="99.999.999/9999-99" value="<?php echo $result['forn_cnpj']; ?>"/><br><br>
         <label>Razão social: </label><input type="text" name="fornecedor_razao" id="fornecedor_razao" value="<?php echo $result['forn_razaosoc']; ?>"/><br><br>
         <label>Rua: </label><input type="text" name="fornecedor_rua" id="fornecedor_rua" value="<?php echo $result['forn_rua']; ?>"/><br><br>
@@ -50,7 +39,7 @@ function editarFornecedor($id){
         <label>País: </label><input type="text" name="fornecedor_pais" id="fornecedor_pais" value="<?php echo $result['forn_pais']; ?>"/><br><br>
         <label>Telefone: </label><input type="text" name="fornecedor_fone" id="fornecedor_fone" placeholder="(99) 91234-5678" value="<?php echo $result['forn_fone']; ?>"/><br><br>
         <label>Email: </label><input type="text" name="fornecedor_email" id="fornecedor_email" placeholder="example@example.com" onblur="validarEmail()" value="<?php echo $result['forn_email']; ?>"/><br><br><br>
-        <button type="submit" name="fornecedor_cadastrar" id="fornecedor_cadastrar">Cadastrar</button>
+        <button type="submit" name="fornecedor_cadastrar" id="fornecedor_cadastrar">Alterar</button>
     </form>
 </fieldset>
 
